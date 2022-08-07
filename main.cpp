489a6e11abcdecdf3f4145c9f59af01bc10d490d
esanc <esancak.hesare@gmail.com>
2022-08-07 20:16:24 +0300

    Version: 0.1.1.1
    1. The file system updated. Now, it is relative to the PROJECT_SOURCE_DIR variable of cmake configurations.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "source/campaign/entity/World.h"
 #include <clocale>
+#include "config.h"
 #include <chrono>
 # include <spdlog/spdlog.h>
 
@@ -9,15 +10,14 @@
 int main(int argc, char* argv[]) {
     std::setlocale(LC_ALL,"Turkish");
 
-    spdlog::set_level(spdlog::level::trace);
+    spdlog::set_level(spdlog::level::debug);
 
     auto start = std::chrono::high_resolution_clock::now();
     Leader::World w(2);
     w.initialize_country();
     auto end = std::chrono::high_resolution_clock::now();
-
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
     std::cout << "Elapsed time is " << duration.count() << " milliseconds\n";
-
+    std::cout << PROJECT_SOURCE_DIR;
     return EXIT_SUCCESS;
 }
--- source/campaign/entity/World.cpp
+++ source/campaign/entity/World.cpp
@@ -3,7 +3,9 @@
 //
 
 #include "World.h"
+#include <iostream>
 #include "spdlog/spdlog.h"
+#include "data/json_file/entity/entity_path.h"
 
 namespace Leader {
 
@@ -15,25 +17,19 @@ namespace Leader {
     World::World(const int &_seed): Entity(generate_world_stringId(_seed)), world_initial_seed(_seed)
     {
         this->world_randomizer.set_seed(this->world_initial_seed);
-        this->world_ideology_axis_objects = load_ideology_axis_objects(
-                R"(E:\Enes_Sancak\Projects\Leader\data\json_file\entity\ideology\ideology_axis.json)",
-                R"(E:\Enes_Sancak\Projects\Leader\data\json_file\entity\ideology\ideology.json)");
+        this->world_ideology_axis_objects = load_ideology_axis_objects(IDEOLOGY_AXIS_PATH, IDEOLOGY_PATH);
         spdlog::info("The world ideology axis objects are loaded.");
         this->ideology_axis_count = world_ideology_axis_objects.size();
-        this->city_population_identity_templates = load_population_identity_template_objects(
-                R"(E:\Enes_Sancak\Projects\Leader\data\json_file\entity\city\city_population_identity_template.json)"
-                );
-        this->party_population_identity_templates = load_population_identity_template_objects(
-                R"(E:\Enes_Sancak\Projects\Leader\data\json_file\entity\party\party_population_identity_template.json)"
-        );
+        this->city_population_identity_templates = load_population_identity_template_objects(CITY_POPULATION_IDENTITY_TEMPLATE_PATH);
+        this->party_population_identity_templates = load_population_identity_template_objects(PARTY_POPULATION_IDENTITY_TEMPLATE_PATH);
         spdlog::info("The city population identity templates are loaded.");
         this->world_country_objects = load_country_objects(
-                R"(E:\Enes_Sancak\Projects\Leader\data\json_file\entity\country\country.json)",
-                R"(E:\Enes_Sancak\Projects\Leader\data\json_file\entity\city\city.json)",
+                COUNTRY_PATH,
+                CITY_PATH,
                 city_population_identity_templates,
                 world_ideology_axis_objects
                 );
-        this->world_character_name_base = load_character_name_base(R"(E:\Enes_Sancak\Projects\Leader\data\json_file\entity\character\character_name_base.json)");
+        this->world_character_name_base = load_character_name_base(CHARACTER_NAME_BASE_PATH);
         spdlog::info("The countries are loaded.");
 
     }
